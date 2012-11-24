19359137755fabec80faae817541d00eb8effd18
jessecurry <jesse@jessecurry.net>
2012-11-24 13:44:15 -0500

    Implement basic functionality

---
+++ html-shell/Defines.h
@@ -0,0 +1,14 @@
+//
+//  Defines.h
+//  html-shell
+//
+//  Created by Jesse Curry on 11/24/12.
+//  Copyright (c) 2012 Haneke Design. All rights reserved.
+//
+
+#ifndef html_shell_Defines_h
+#define html_shell_Defines_h
+
+
+
+#endif
